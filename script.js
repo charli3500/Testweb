a46e035417e3ed47941f06dff9cf2fc07e2fe725
charli3500 <169071434+charli3500@users.noreply.github.com>
2025-01-18 13:47:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("irofb5").addEventListener("click", function() {
-    const webhookURL = "https://discord.com/api/webhooks/1330153787788361739/t-XAdD4S2W9JM7QLmLZ_LDBGjmyFS1heGCs6HA3zlSWljkwE8tE4eZ5k6mEhCRZWvtK6";
+    const webhookURL = "removed4now";
     const userInput = document.getElementById("SendText").value; // Capture user input from the text input field
 
     // Fetch the client's IP address using ipify API
